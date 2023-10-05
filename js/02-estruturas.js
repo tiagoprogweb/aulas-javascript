3918e28e9ad11d714b941df39256d012e3c58ca5
tiago-progwebt23 <tiago.progwebt23b@gmail.com>
2023-10-05 16:15:19 -0300

    Enunciado exercícios

--- js/02-estruturas.js
+++ js/02-estruturas.js
@@ -8,8 +8,15 @@ Lista de dados indexados, sequenciais e acessíveis por um
 Lista de dados não indexados e formados por propriedades
 e valores. */
 
-// Arrays
+// Arrays (os índices sempre começam no zero)
 let cursos = ["Node.js", "React", "SQL", "UX/UI Design"];
 
-console.log(cursos); // saída direta/estruturada
+console.log(cursos); // saída direta/estruturada (APENAS NO console)
 console.log(cursos[2]); // SQL
+
+/* Exercícios
+1) Crie uma constante chamada "dados" e coloque nela
+os dados de uma pessoa: nome, sobrenome, idade, telefone e cidade 
+
+2) Mostre no console uma mensagem contendo somente o nome e a idade
+da pessoa. Exemplo: "Fulano tem 25 anos." */
