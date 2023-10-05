0fb69375e529dfc8c0b768913fdca3b8bb87d2fa
tiago-progwebt23 <tiago.progwebt23b@gmail.com>
2023-10-05 15:03:43 -0300

    Sintaxe geral do JS

---
+++ js/01-sintaxe.js
@@ -0,0 +1,31 @@
+console.log("Programador Web");
+console.log('Senac Penha');
+
+/* Comentário - ALT SHIFT A */
+// Comentário - CTRL ;
+
+/* Manipulando dados */
+
+// Variáveis
+let curso = "HTML5 e CSS3";
+let ano = 2023;
+
+// Constantes
+const mediaMinima = 7;
+
+// Saída de dados a partir da memória
+console.log(curso);
+console.log(mediaMinima);
+console.log(ano);
+
+
+// Saída de dados personalizada
+
+/* Estou fazendo o curso de HTML5 e CSS3
+neste ano de 2023. */
+
+// Template String/Literal: saída usando crases e ${}
+console.log(`Estou fazendo o curso de ${curso} neste ano de ${ano}.`);
+
+// Concatenação (forma mais antiga de saída, mas ainda válida)
+console.log("Estou fazendo o curso de "+curso+" neste ano de "+ano+".");
