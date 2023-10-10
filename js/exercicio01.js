a9f765cc72a3c0a68d7795bedd436430b7b058ff
tiago-progwebt23 <tiago.progwebt23b@gmail.com>
2023-10-10 17:30:00 -0300

    Update exercicio01.js

--- js/exercicio01.js
+++ js/exercicio01.js
@@ -1,17 +1,29 @@
+/* Habilitando o modo estrito do JS */
+"use strict";
+
 // 3) crie duas variáveis: nota1 e nota2
-let nota1 = 6; // valor de 0 a 10
+let nota1 = 7; // valor de 0 a 10
 let nota2 = 6; // valor de 0 a 10
 
 // 4) variável média que é calculada
 let media = (nota1 + nota2) / 2;
 console.log(media);
 
+
+let resultado;
+
 // 5) Condicional (aprovado, recuperação ou reprovado)
 if(media >= 7){
-    console.log("aprovado");
+    resultado = "aprovado";
 } else if(media >= 5){
-    console.log("recuperação");
+    resultado = "recuperação";
 } else {
-    console.log("reprovado");
+    resultado = "reprovado";
 }
+
+console.log(resultado);
+
+
+
+
 
