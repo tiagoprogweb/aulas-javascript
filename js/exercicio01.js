a9c2997fc458c06677bb7dfc6b196d71ac9cfd61
tiago-progwebt23 <tiago.progwebt23b@gmail.com>
2023-10-10 17:16:39 -0300

    Correção

---
+++ js/exercicio01.js
@@ -0,0 +1,17 @@
+// 3) crie duas variáveis: nota1 e nota2
+let nota1 = 6; // valor de 0 a 10
+let nota2 = 6; // valor de 0 a 10
+
+// 4) variável média que é calculada
+let media = (nota1 + nota2) / 2;
+console.log(media);
+
+// 5) Condicional (aprovado, recuperação ou reprovado)
+if(media >= 7){
+    console.log("aprovado");
+} else if(media >= 5){
+    console.log("recuperação");
+} else {
+    console.log("reprovado");
+}
+
