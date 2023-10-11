81977a3a79485c5820145c1364a4ca915f3ece18
tiago-progwebt23 <tiago.progwebt23b@gmail.com>
2023-10-11 16:20:19 -0300

    Loop do/while

--- js/05-loops.js
+++ js/05-loops.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /* Comandos de Repetição
 Instruções para realizar ações
 por uma quantidade específica de vezes.
@@ -10,8 +12,17 @@ Comandos tradicionais:
 Obs.: normalmente o loop é controlado
 através de uma variável contadora. */
 
-let contador = 1;
+// EXEMPLO WHILE (ENQUANTO)
+/* let contador = 1;
 while( contador <= 5 ){
     console.log(`Valor de contador: ${contador}`);
     contador++; // ++ incremento
-}
+} */
+
+
+// EXEMPLO DO/WHILE (FAÇA/ENQUANTO)
+let contador = 1;
+do {
+    console.log(`Contador vale: ${contador}`);
+    contador++;
+} while( contador <= 3 )
