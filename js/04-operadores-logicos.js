5e0e27f41279dac98b0f56865ddbecd5b2203909
tiago-progwebt23 <tiago.progwebt23b@gmail.com>
2023-10-11 14:37:38 -0300

    Update 04-operadores-logicos.js

--- js/04-operadores-logicos.js
+++ js/04-operadores-logicos.js
@@ -6,16 +6,25 @@ const limiteDeFaltas = 10;
 
 // Resultados do aluno
 let aluno = "Jean";
-let mediaFinal = 8.5;
-let faltas = 3;
+let mediaFinal = 6;
+let faltas = 11;
 let resultado;
 
 /* Operador lógico && (AND - E) */
+// if(mediaFinal >= mediaMinima && faltas <= limiteDeFaltas){
+//     resultado = "aprovado";
+// } else {
+//     resultado = "reprovado";
+// }
+
 if(mediaFinal >= mediaMinima && faltas <= limiteDeFaltas){
     resultado = "aprovado";
+} else if(faltas > limiteDeFaltas){
+    resultado = "reprovado por faltas";
 } else {
     resultado = "reprovado";
 }
+
 
 console.log(`O aluno ${aluno} está ${resultado}`);
 console.log(`Média do aluno: ${mediaFinal}`);
