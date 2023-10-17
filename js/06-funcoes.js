a64d3a9e6144b2a0606d07824c87e6b06ce77032
tiago-progwebt23 <tiago.progwebt23b@gmail.com>
2023-10-17 15:19:38 -0300

    Exemplos de funções

--- js/06-funcoes.js
+++ js/06-funcoes.js
@@ -12,3 +12,40 @@ ponto de vista de organização de código.
 
 O JS já possui centenas de funções prontas. 
 Exemplos: log(), alert(), prompt(), toFixed() etc */
+
+/* Como escrever funções no JS? */
+
+// Forma 1: função anônima
+const exemplo1 = function(){
+    console.log("Função anônima");
+};
+
+// Forma 2: função nomeada/declarada
+function exemplo2(){
+    console.log("Função nomeada/declarada");
+}
+
+// Forma 3: arrow function
+const exemplo3 = () => {
+    console.log("Arrow Function");
+};
+
+// Chamada de função para execução
+exemplo1();
+exemplo2();
+exemplo3();
+
+/* Exemplos diversos */
+
+/* Função com parâmetros e retorno
+de dados/resultados */
+function somar(valor1, valor2){
+    return valor1 + valor2;
+}
+
+/* Chamamos a função diversas vezes
+e passando valores diferentes como
+parâmetros. */
+console.log( somar(10, 20) );
+console.log( somar(15, 50) );
+console.log( somar(30, 47.8) );
