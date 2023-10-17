8790d593294888fb93561cf8f583c19b0d5f5908
tiago-progwebt23 <tiago.progwebt23b@gmail.com>
2023-10-17 16:40:36 -0300

    Update 06-funcoes.js

--- js/06-funcoes.js
+++ js/06-funcoes.js
@@ -57,10 +57,12 @@ let desconto = preco * 0.10; // 10%
 let precoFinal = preco - desconto;
 
 function formatarPreco(valor){
-    return valor.toLocaleString("pt-br", {
+    let opcoes = {
         style: "currency",
         currency: "BRL"
-    })
+    };
+
+    return valor.toLocaleString("pt-br", opcoes);
 }
 
 console.log(`Preço: ${formatarPreco(preco)}`);
