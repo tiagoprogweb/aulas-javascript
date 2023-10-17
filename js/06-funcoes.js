714b66d62a84a35af90fc6282a5d2ec19b9300ae
tiago-progwebt23 <tiago.progwebt23b@gmail.com>
2023-10-17 14:53:51 -0300

    Conceitos sobre funções

---
+++ js/06-funcoes.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/* Sobre funções
+
+Funções são blocos de código que
+podem ser executados e reaproveitados
+em praticamente qualquer lugar de
+uma aplicação/site.
+
+Usar funções também é benéfico do
+ponto de vista de organização de código.
+
+O JS já possui centenas de funções prontas. 
+Exemplos: log(), alert(), prompt(), toFixed() etc */
