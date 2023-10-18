65738634eb0c4900c8a8590cee65bb3365ac8efb
tiago-progwebt23 <tiago.progwebt23b@gmail.com>
2023-10-18 14:34:57 -0300

    Correção exercício 03

---
+++ js/exercicio03.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// Dados de entrada (exemplos de valores)
+let valorDaCotacaoDoDia = 5.03;
+let valorEmDolares = 2500;
+
+// Função que realiza a conversão matemática ao receber PARÂMETROS
+function converteMoeda( valor, cotacao ){
+    return valor * cotacao;
+}
+
+// Variável que receberá o resultado processado pela função DEPOIS que
+// a função terminar sua execução
+let valorConvertido = converteMoeda(valorDaCotacaoDoDia, valorEmDolares);
+
+// Saídas após o processamento
+console.log(valorEmDolares); // quanto que era em dólares
+console.log(valorConvertido); // quanto que fica em reais
+
