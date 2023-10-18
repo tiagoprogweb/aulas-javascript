7040b4420c0b58040efdcd7f3dc0f013709d1a82
tiago-progwebt23 <tiago.progwebt23b@gmail.com>
2023-10-18 17:01:45 -0300

    Manipulação do DOM

---
+++ js/07-dom-eventos.js
@@ -0,0 +1,46 @@
+"use strict";
+
+/* Funções de seleção no DOM
+
+-getElementById()
+Seleciona UM elemento através de um id.
+
+-querySelector()
+Seleciona UM elemento através de um seletor.
+
+-querySelectorAll()
+Seleciona VÁRIOS elementos através de um/vários seletor(es) */
+
+/* Exemplos */
+
+// Acessando o DOM
+const subtitulo = document.getElementById("subtitulo-dom");
+const desenho = document.querySelector("figure");
+const descricao = document.querySelector("#descricao");
+
+console.log(desenho);
+console.log(descricao);
+
+const variosElementos = document.querySelectorAll("h3, a");
+console.log(variosElementos);
+console.log(variosElementos[1]);
+
+// Modificando o DOM
+const titulo = document.querySelector("h1");
+titulo.textContent = "Olá JavaScript!";
+titulo.style.textAlign = "center";  // CSS inline via JS
+titulo.classList.add("destaque"); // Classe CSS via JS
+
+/* Selecionando os links contidos na lista */
+const linksReferencia = document.querySelectorAll("ul li a");
+console.log(linksReferencia);
+
+let quantidadeDeLinks = linksReferencia.length;
+
+for(let link of linksReferencia){
+    link.setAttribute("target", "_blank");
+}
+
+// for( let i = 0; i < quantidadeDeLinks; i++ ){
+//     linksReferencia[i].setAttribute("target", "_blank");
+// }
